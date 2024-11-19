4a96153c78d1398c3e9ef36bc0be54afda08f712
nhannguyentruc214 <nhannguyentruc214@gmail.com>
2024-11-19 11:11:45 +0700

    n-2-commit

--- polls/admin.py
+++ polls/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import Customer, Product, OrderHistory, OrderDetail
+from .models import Customer, Product, Order, OrderDetail
 from django.db.models import Count
 
 class OrderInline(admin.StackedInline):
-    model = OrderHistory
+    model = Order
     extra = 1
     show_change_link = True
 
@@ -34,31 +34,35 @@ class ProductNameFilter(admin.SimpleListFilter):
     
 @admin.register(Customer)
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = ('name','address')
+    list_display = ('name','address','contact_info')
     inlines = [OrderInline]
 
-@admin.register(OrderHistory)
-class OrderHistoryAdmin(admin.ModelAdmin):
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
 
-    list_display = ('order_date','id','status','total_ammount','total_order_value')
-    list_filter = ('order_date','id','status')
-    readonly_fields = ('total_ammount','total_order_value')
+    def get_order_id(self, obj):
+        return obj.id
+    
+    list_display = ('order_date','customer_name','get_order_id','total_amount','total_order_price')
+    list_filter = ('order_date','id',)
+    readonly_fields = ('total_amount','total_order_price')
+    get_order_id.short_description='Order Id'
     inlines = [OrderDetailInline]
 
 @admin.register(OrderDetail)
 class OrderDetailAdmin(admin.ModelAdmin):
     
-    def get_order_history_id(self, obj):
-        return obj.orderHistory_id
+    def get_order_id(self, obj):
+        return obj.order_id
     
-    list_display = ('product_name', 'get_order_history_id','quantity','total_quantity_value',)
-    readonly_fields = ('product_name', 'orderHistory_id','total_quantity_value',)
-    get_order_history_id.short_description='Order History Id'
+    list_display = ('product_name','customer_name', 'get_order_id','quantity','price',)
+    readonly_fields = ('price',)
+    get_order_id.short_description='Order Id'
     list_filter=(ProductNameFilter,)
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name','value','description')
+    list_display = ('name','price','description','stock_quantity')
     list_filter = ('name',)
     inlines = [OrderDetailInline]
 
--- polls/models.py
+++ polls/models.py
@@ -3,60 +3,60 @@ from django.db import models
 class Customer(models.Model):
     name = models.CharField(max_length=255)
     address = models.TextField()
+    contact_info = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
-        return self.name
+        return f"{self.name}"
     
 class Product(models.Model):
     name = models.CharField(max_length=255, unique=True)
-    value = models.DecimalField(max_digits=10, decimal_places=2)  # Price of the product
+    price = models.DecimalField(max_digits=10, decimal_places=2)  # Price of the product
     description = models.TextField()
+    stock_quantity = models.DecimalField(max_digits=5, decimal_places=2)
     def __str__(self):
-        return self.name
+        return f"{self.name}"
 
     
-class OrderHistory(models.Model):
-    CHOICES = {
-        "RF": "Refunded",
-        "CP": "Completed",
-        "CN": "Cancelled",
-        "AW": "Awaiting Payment"
-    }
+class Order(models.Model):
     order_date = models.DateField()
-    status = models.CharField(max_length=255, choices=CHOICES)
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='order_histories')
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='orders')
 
     @property
-    def total_order_value(self):
-        # Calculate the total value by summing the `value` field of related products
-        # total = self.orderDetails.aggregate(total=models.Sum('total_quantity_value'))['total']
-        total = sum(detail.total_quantity_value for detail in self.orderDetails.all())
+    def total_order_price(self):
+        total = sum(detail.price for detail in self.order_details.all())
         return total or 0  # Return 0 if no products are associated
     
     @property
-    def total_ammount(self):
-        # Calculate the total value by summing the `value` field of related products
-        ammount = self.orderDetails.aggregate(total=models.Sum('quantity'))['total']
-        return ammount or 0  # Return 0 if no products are associated
+    def total_amount(self):
+        amount = self.order_details.aggregate(total=models.Sum('quantity'))['total']
+        return amount or 0  # Return 0 if no products are associated
+    
+    @property
+    def customer_name(self):
+        return self.customer.name or None  # Return 0 if no products are associated
     
     def __str__(self):
-        return f"Order {self.pk}: {self.customer.name} - {self.status}"
+        return f"Order {self.pk} - Customer {self.customer.name}"
    
 class OrderDetail(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='ordered_products')
-    orderHistory = models.ForeignKey(OrderHistory, on_delete=models.CASCADE, related_name='orderDetails')
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='order_details')
     quantity = models.IntegerField()
     
     @property
-    def total_quantity_value(self):
-        # Calculate the total value by summing the `value` field of related products
-        total = self.product.value * self.quantity
+    def price(self):
+        # Calculate the total price by summing the `price` field of related products
+        total = self.product.price * self.quantity
         return total or 0  # Return 0 if no products are associated
         
     @property
     def product_name(self):
         return self.product.name or None  # Return 0 if no products are associated
-
+   
+    @property
+    def customer_name(self):
+        return self.order.customer.name or None  # Return 0 if no customers are associated
+    
     def __str__(self):
-        return f"Order detail {self.pk}"
+        return f"#{self.pk}"
     
